812c5d3bfffb47433587b4263675876c01cc79f8
John Prescott <jprescottirl@gmail.com>
2020-04-26 13:15:32 -0700

    checlletter function

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const startDiv = document.querySelector('.start');
 const title = document.querySelector('.title');
-const olay = document.getElementById('overlay');
+const overlay = document.getElementById('overlay');
+
 let missed = 0;
 
 
@@ -35,6 +36,8 @@ qwerty.addEventListener('click', (e) => {
           
         }
     }
+
+    
    
 });
 
@@ -65,7 +68,6 @@ getRandomPhraseAsArray(wordPhrase);
 // adds the letter of a string to the display
 
 const addPhraseToDisplay = (arr) => {
-    let phraseDisplay = document.querySelector('ul');
     for (let i = 0; i < arr.length; i++) {
       let li = document.createElement('li');
       li.textContent = arr[i];
@@ -77,7 +79,7 @@ const addPhraseToDisplay = (arr) => {
         li.classList.add('letter');
       }
 
-      phraseDisplay.appendChild(li);
+      phrase.appendChild(li);
     }
     
 }
@@ -90,12 +92,12 @@ addPhraseToDisplay(phraseArray);
 // Check if a letter is in the phrase
 
 const checkLetter = (button) => {
-    let listItems = document.querySelectorAll('.letter');
+    let listItems = document.getElementsByClassName('letter');
     let matchFound = null;
 
     for (i = 0; i < listItems[i].length; i++) {
         
-        if (button.textContent === listItems[i].textContent) {
+        if (listItems[i].textContent === button) {
             listItems.classList.add('show');
             matchFound = listItems[i].textContent;
            
@@ -108,25 +110,28 @@ const checkLetter = (button) => {
 
 
 const checkWin = () => {
-    let showLetter = document.getElementsByTagName('li').className = 'letter';
-    let showItems = document.getElementsByTagName('li').className = 'show';
-
+    const showLetter = document.getElementsByClassName('letter');
+    const showItems = document.getElementsByClassName('show');
     if (showLetter.length === showItems.length) {
-        startDiv.classList.add('win');
-        startDiv.style.display = '';
-        title.textContent = 'You win!';
+        overlay.classList.add('win');
+        overlay.style.display = 'flex';
+        title.textContent = 'You Won!';
+        
 
 
-
-     } else if (missed >= 5) {
-            startDiv.classList.add('lost');
-            startDiv.style.display = '';
+    } 
+    
+    
+    if (missed > 4 ) {
+            overlay.classList.add('lose');
+            overlay.style.display = 'flex';
             title.textContent = 'You Lose!';
+            
             
 
         
-
+        checkWin();
     }
+    
 
-    checkWin(olay);
 }
